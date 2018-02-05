b79da3de7e2e16452fe13f5a8efc328b81473a0b
Austen Ishii <austenishii@gmail.com>
2018-02-05 15:54:11 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 const app = "I don't do much."
+function destructivelyAppendKitten(name) {kittens.push("Ralph")}
+function destructivelyPrependKitten(name) {kittens.unshift("Bob")}
+function destructivelyRemoveLastKitten() {kittens.pop()}
+function destructivelyRemoveFirstKitten() {kittens.shift()}
+function appendKitten(name) { var appendedkittens = [...kittens, "Broom"]; return appendedkittens}
+function prependKitten(name) { var prependkittens = ["Arnold", ...kittens]; return prependkittens}
+function removeLastKitten() { var removekitten = kittens.slice(2); return removekitten}
